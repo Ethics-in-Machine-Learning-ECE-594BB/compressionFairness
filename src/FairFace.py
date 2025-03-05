0e77b58f05834db4c85a811e48bd399d012d6682
Jack <jackdshoemaker@gmail.com>
2025-03-04 23:09:40 -0800

    Changing face dataset, adding diff bit sizes

---
+++ src/FairFace.py
@@ -0,0 +1,47 @@
+""" Instructions for setting up FairFace data locally:
+1. git clone https://huggingface.co/datasets/HuggingFaceM4/FairFace
+2. In fairface directory git lfs install
+3. git lfs pull 
+4. Run the following code
+import pandas as pd
+from PIL import Image
+import io
+df = pd.read_parquet("your_path.../FairFace/0.25/train-00000-of-00002-d405faba4f4b9b85.parquet")
+for i in range(9000):
+    im = df.iloc[i]['image']
+    image = Image.open(io.BytesIO(im['bytes']))
+    image.save(f"your_path.../CompressionFairness/data/images/train/faces/{im['path']}")
+for i in range(9001,15000):
+    im = df.iloc[i]['image']
+    image = Image.open(io.BytesIO(im['bytes']))
+    image.save(f"your_path.../CompressionFairness/data/images/test/faces/{im['path']}")    
+5. Install: https://www.kaggle.com/datasets/jessicali9530/caltech256
+6. Run the following code: 
+import os 
+import random
+from math import floor
+from tqdm import tqdm
+source = 'your_path.../256_ObjectCategories/256_ObjectCategories/'
+destination = 'your_path.../compressionFairness/data/images/train/non-faces'
+allimages = os.listdir(source)
+for dir in tqdm(allimages):
+    curr_dir = os.path.join(source, dir)
+    new_dir = os.listdir(curr_dir)
+    index = floor(len(new_dir)*0.6)
+    for img in new_dir[:index]:
+        src_path = os.path.join(curr_dir, img)
+        dst_path = os.path.join(destination, img)
+        os.rename(src_path, dst_path)
+destination = 'your_path.../compressionFairness/data/images/test/non-faces'
+for dir in tqdm(allimages):
+    curr_dir = os.path.join(source, dir)
+    new_dir = os.listdir(curr_dir)
+    index = floor(len(new_dir)*0.6)
+    for img in new_dir[index:]:
+        src_path = os.path.join(curr_dir, img)
+        dst_path = os.path.join(destination, img)
+        os.rename(src_path, dst_path)
+Might be a more efficient way of doing this but idk
+"""
+import pandas as pd 
+# TODO: evaluation loader for working with AIF360
--- src/quantization.py
+++ src/quantization.py
@@ -2,22 +2,41 @@ import torch
 import torch.quantization as quantization
 import os 
 from src.simpleFCNN import SimpleFCNN
-# load quantized model 
+from torch.ao.quantization.observer import MinMaxObserver, MovingAverageMinMaxObserver
+
+class myObserver(MinMaxObserver):
+    def __init__(self, bit_width=8, **kwargs):
+        super().__init__(**kwargs)
+        self.bit_width = bit_width
+    def calculate_qparams(self):
+        scale, zero_point = super().calculate_qparams()
+        qmin, qmax = 0, (2**self.bit_width) - 1
+        return scale, zero_point.clamp(qmin, qmax)
 
 # torch optimization for fusing quantized layers together, helps with inference speed
 # dependent on model architecture needs changing for other models 
 def fuse_modules(model): 
     quantization.fuse_modules(model, [['fc1', 'relu']],  inplace=True)
 
-def compute_model_size_in_memory(model):
-    total_size = sum(p.element_size() * p.numel() for p in model.parameters())
-    return total_size  # Convert to MB
+def compute_model_size_in_memory(model, bit_width):
+    param_size = 0
+    # iterate through params and buffers summing up each
+    for param in model.parameters():
+        param_size += param.nelement()* param.element_size()
+    buffer_size = 0
+    try:
+        for buffer in model.buffer():
+            buffer_size += buffer.nelement()*buffer.element_size()
+    except:
+        # do nothing
+        buffer_size = 0
+    return (param_size+buffer_size)/(1024*1024)  # Convert to MB
 
-def static_quantization(quantized_model,test_loader, path):
-    quantized_model.qconfig = quantization.default_qconfig
+def static_quantization(quantized_model,test_loader, path, bit_width=8):
+    quantized_model.qconfig = quantization.QConfig(activation=myObserver.with_args(bit_width=bit_width, ), weight=quantization.default_weight_observer)
+    # quantized_model.qconfig = quantization.default_qconfig
 
     # fuse_modules(unquantized_model)
-    
     quantization.prepare(quantized_model, inplace=True)
     # need to pass some test data through model to allow torch to calibrate quantization 
     with torch.no_grad():
@@ -26,8 +45,6 @@ def static_quantization(quantized_model,test_loader, path):
     quantization.convert(quantized_model, inplace=True)
     print("Post-Train Quantization Complete")
     torch.save(quantized_model.state_dict(), path)
-    # orig_size = compute_model_size_in_memory(unquantized_model)
-    # quantized_size = compute_model_size_in_memory(quantized_model.to('cpu'))
-    # print(f"Original Model Size in RAM: {orig_size:.2f} MB")
-    # print(f"Quantized Model Size in RAM: {quantized_size:.2f} MB")
+    quantized_size = compute_model_size_in_memory(quantized_model.to('cpu'))
+    print(f"Quantized size: {quantized_size}")
 # https://gist.github.com/martinferianc/d6090fffb4c95efed6f1152d5fde079d 
